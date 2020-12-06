13b8f0f97f921633fa35ec82c2bce47d6186b611
Dilan Nana <dilannana.js@gmail.com>
2020-12-06 02:48:10 -0800

    add to card 2.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,41 @@
 import React from "react";
-import Product from "./components/Product";
-import data from "./data";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-
-import ProductScreen from "./screens/ProductScreen";
-import HomeScreen from "./screens/HomeScreen";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Link, Route } from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
-    <Router>
+    <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
-              MernMarket
-            </a>
+            <Link className="brand" to="/">
+              amazona
+            </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
-          <Route exact path="/" component={HomeScreen} />
-          <Route exact path="/cart/:id?" component={CartScreen} />
-          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
         </main>
-        <footer className="row center">All rights reserved</footer>
+        <footer className="row center">All right reserved</footer>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
